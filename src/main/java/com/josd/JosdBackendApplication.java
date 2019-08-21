df2a393558af36098826e8069983ad5673a6a6ec
Gwangwoo Kim <gwkim8107@tourbaksa.co.kr>
2019-08-21 21:44:39 +1200

    initial commit

---
+++ src/main/java/com/josd/JosdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.josd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosdBackendApplication.class, args);
+	}
+
+}
