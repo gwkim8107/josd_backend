deaa7eed50f62dfcb192c9b4502c231405a016ab
Gwangwoo Kim <gwkim8107@tourbaksa.co.kr>
2019-08-24 00:07:11 +1200

    Initial commit

---
+++ src/main/java/josd/JosdApiApplication.java
@@ -0,0 +1,13 @@
+package josd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/josd/JosdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package josd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
