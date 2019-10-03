9f2b78e67312cbb14d95e776865189c8f7f00c24
Gwangwoo Kim <gwkim8107@tourbaksa.co.kr>
2019-10-04 10:51:59 +1300

    Small fixation of UserController

--- src/main/java/josd/login/controller/UsersController.java
+++ src/main/java/josd/login/controller/UsersController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,7 +39,7 @@ public class UsersController {
 		logger.info("Creating User : {}", user);
 
 		userService.registerUser(user);
-		return new ResponseEntity<String>("Success", HttpStatus.CREATED);
+		return new ResponseEntity<String>("Success", HttpStatus.ACCEPTED);
 	}
 	
 	@CrossOrigin(origins = "*", maxAge = 3600)
