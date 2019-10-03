07f7da2d2ce78a08695698e456300690ce4d8ad9
Gwangwoo Kim <gwkim8107@tourbaksa.co.kr>
2019-10-04 10:53:25 +1300

    Completed with Rest API for '/reading'

---
+++ src/main/java/josd/main/controller/MainActivityContoller.java
@@ -0,0 +1,108 @@
+package josd.main.controller;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import josd.main.entity.Chant;
+import josd.main.entity.Reading;
+import josd.main.service.MainActivityService;
+
+@RestController
+public class MainActivityContoller {
+	public static final Logger logger = org.slf4j.LoggerFactory.getLogger(MainActivityContoller.class);
+	
+	@Autowired
+	private MainActivityService mainService;
+	
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping(path = "/chant")
+	public ResponseEntity<?> saveChant(@RequestBody Chant chant){
+//		Round Example 
+//		int a = (int) Math.round(6.14); // 3 
+//		int b = (int) Math.round(6.99); // 4 
+//		int c = (int) Math.round(6.5); // 4
+		
+		
+//		logger.info("Check Chant data : {}", chant);
+		String user_id = chant.getUser_id();
+		String rec_dt = chant.getRec_dt();
+		
+		// 12:00 AM - 8:00 AM
+		int bf_8am = chant.getBf_8am();
+		int point_bf_8am = (int) Math.round(bf_8am * 4.5);
+		chant.setPoint_bf_8am(point_bf_8am);
+		
+		// 8:00 AM - 6:00 PM
+		int btw_8to6pm = chant.getBtw_8to6pm();
+		int point_btw_8to6pm = (int) Math.round(btw_8to6pm * 3);
+		chant.setPoint_btw_8to6pm(point_btw_8to6pm);
+		
+		// 6PM - 12AM
+		int af_6pm = chant.getAf_6pm();
+		int point_af_6pm = (int) Math.round(af_6pm * 1.5);
+		chant.setPoint_af_6pm(point_af_6pm);
+		
+		// Total
+		int chant_total = bf_8am + btw_8to6pm + af_6pm;
+		int point_chant_total = point_bf_8am + point_btw_8to6pm + point_af_6pm;
+		if( point_chant_total > 72 ) {
+			point_chant_total = 72;
+		}
+		chant.setChant_total(chant_total);
+		chant.setPoint_total(point_chant_total);
+		
+		// Check the data existence
+		int chant_count = mainService.checkChantData(user_id, rec_dt);
+		System.out.println("count = "+chant_count);
+		
+		if (chant_count > 0) {
+			// Update data
+			mainService.updtChant(chant);
+		}else if(chant_count == 0) {
+			// Insert data
+			mainService.saveChant(chant);
+		}
+		
+		return new ResponseEntity<String>("Success", HttpStatus.ACCEPTED);
+	}
+	
+	@CrossOrigin(origins="*", maxAge=3600)
+	@RequestMapping(path="/reading")
+	public ResponseEntity<?> saveReading(@RequestBody Reading reading){
+		String user_id = reading.getUser_id();
+		String rec_dt = reading.getRec_dt();
+		
+		int sub_dura = reading.getSub_dura();
+		int sub_point = 0;//
+		if(sub_dura < 15) {
+			sub_point = -20;
+		}else if(sub_dura >= 15) {
+			sub_point = (int) Math.round( (sub_dura * 18) / 60 );
+		}
+		if(sub_point > 18) {
+			sub_point = 18;
+		}
+		reading.setSub_point(sub_point);
+		
+		// Check the data existence
+		int reading_count = mainService.checkReadData(user_id, rec_dt);
+		System.out.println("reading_count = "+reading_count);
+		
+		if (reading_count > 0) {
+			// Update data
+			mainService.updtReading(reading);
+		}else if(reading_count == 0) {
+			// Insert data
+			mainService.saveReading(reading);
+		}
+		
+		return new ResponseEntity<String>("Success", HttpStatus.ACCEPTED);
+		
+	}
+}
---
+++ src/main/java/josd/main/entity/Reading.java
@@ -0,0 +1,79 @@
+package josd.main.entity;
+
+public class Reading {
+	private String user_id;
+	private String rec_dt;
+	private String sub_area;
+	private String sub_name;
+	private String sub_detail;
+	private int sub_dura;
+	private int sub_point;
+	private String up_dtime;
+	private String up_id;
+	private String user_id_1;
+	
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getRec_dt() {
+		return rec_dt;
+	}
+	public void setRec_dt(String rec_dt) {
+		this.rec_dt = rec_dt;
+	}
+	public String getSub_area() {
+		return sub_area;
+	}
+	public void setSub_area(String sub_area) {
+		this.sub_area = sub_area;
+	}
+	public String getSub_name() {
+		return sub_name;
+	}
+	public void setSub_name(String sub_name) {
+		this.sub_name = sub_name;
+	}
+	public String getSub_detail() {
+		return sub_detail;
+	}
+	public void setSub_detail(String sub_detail) {
+		this.sub_detail = sub_detail;
+	}
+	public int getSub_dura() {
+		return sub_dura;
+	}
+	public void setSub_dura(int sub_dura) {
+		this.sub_dura = sub_dura;
+	}
+	public int getSub_point() {
+		return sub_point;
+	}
+	public void setSub_point(int sub_point) {
+		this.sub_point = sub_point;
+	}
+	public String getUp_dtime() {
+		return up_dtime;
+	}
+	public void setUp_dtime(String up_dtime) {
+		this.up_dtime = up_dtime;
+	}
+	public String getUp_id() {
+		return up_id;
+	}
+	public void setUp_id(String up_id) {
+		this.up_id = up_id;
+	}
+	public String getUser_id_1() {
+		return user_id_1;
+	}
+	public void setUser_id_1(String user_id_1) {
+		this.user_id_1 = user_id_1;
+	}
+	
+	
+	
+}
